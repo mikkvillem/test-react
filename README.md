276f789d47156f86e5ea1529191ec246823200aa
mikk-sisutech <mikk.villem@sisugroup.com>
2024-10-28 21:50:41 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# React test
+
+-   Create a custom hook useCout()
+
+-   Use the hook to implement increment, decrement, incrementBy, incrementDebounced, onCountChange
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+// import useCount from './hooks/useCount';
+function App() {
+    //onCountChange(callback)
+    return (
+        <>
+            <div>
+                <h1>Count: 0</h1>
+            </div>
+
+            <button onClick={() => {}}>increment</button>
+
+            <button onClick={() => {}}>decrement</button>
+
+            <button onClick={() => {}}>incement by 3</button>
+
+            <button onClick={() => {}}>incement debounced</button>
+        </>
+    );
+}
+
+export default App;
