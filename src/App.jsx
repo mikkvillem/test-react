a632e40396d7addf8be373b69b466a8d5e089c19
mikk-sisutech <mikk.villem@sisugroup.com>
2024-10-30 14:56:25 +0200

    modify descriptions

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import './App.css';
 // import useCount from './hooks/useCount';
 function App() {
-    //onCountChange(callback)
     return (
         <>
             <div>
                 <h1>Count: 0</h1>
             </div>
-
             <button onClick={() => {}}>increment</button>
-
             <button onClick={() => {}}>decrement</button>
-
             <button onClick={() => {}}>incement by 3</button>
-
-            <button onClick={() => {}}>incement debounced</button>
+            <button onClick={() => {}}>incement debounced</button> {/* increment by num of clicks only 300ms after latest click */}
         </>
     );
 }
+// Change the background color of count each time the count changes.
 
 export default App;
